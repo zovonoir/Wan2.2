927d0867cda8dd5b84dc733199a20b0ea2f89b1c
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-06 13:11:04 +0800

    modify test script

--- run_perf.sh
+++ run_perf.sh
@@ -1,7 +1,7 @@
 docker rm -f zov_wan2.2_rope_alltoall_fusion_test
-docker rmi wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460
+docker rmi wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2
 
-docker build -f dockerfile_rocm_7.0.2 -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460 .
+docker build -f dockerfile_rocm_7.0.2 -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2 .
 
 docker run  -d \
         --network=host \
@@ -15,7 +15,7 @@ docker run  -d \
         --ulimit stack=67108864 \
         -v /home/jialzhu:/home/jialzhu \
         --name zov_wan2.2_rope_alltoall_fusion_test \
-        -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460
+        -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2
 
 docker restart zov_wan2.2_rope_alltoall_fusion_test
 sleep 3
