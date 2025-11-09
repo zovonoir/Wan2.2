281dbe0a9a131d9c4821238618b518cf150897d0
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-09 15:24:12 +0800

    mismatch solved! collect profler for optimized code remove clone add all to all backward

--- wan/distributed/sequence_parallel.py
+++ wan/distributed/sequence_parallel.py
@@ -453,10 +453,10 @@ def sp_attn_forward(self, x, seq_lens, grid_sizes, freqs_i, dtype=torch.bfloat16
         window_size=self.window_size,
     )
 
-    # x = all_to_all(x, scatter_dim=1, gather_dim=2)
-    triton_all_to_all_4D_bf16_backward[(sp_seq_len,1,1)](x,hs,hn//world_size,sp_seq_len*world_size,rank,world_size,iris_o,heap_bases)
+    x = all_to_all(x, scatter_dim=1, gather_dim=2)
+    # triton_all_to_all_4D_bf16_backward[(sp_seq_len,1,1)](x,hs,hn//world_size,sp_seq_len*world_size,rank,world_size,iris_o,heap_bases)
     x = x.flatten(2)
-    shmem_handle.barrier()
+    # shmem_handle.barrier()
  
     x = self.o(x)
     return x
