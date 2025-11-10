4c1e4ec8d4aa901eac0a2074f65b2f063054fb8c
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-10 12:02:46 +0800

    collect profile with shape, debug error gap,collect optimized perf  with callstack

--- run_perf.sh
+++ run_perf.sh
@@ -20,7 +20,7 @@ docker run  -d \
 docker restart zov_wan2.2_rope_alltoall_fusion_test
 docker exec zov_wan2.2_rope_alltoall_fusion_test git -C /app/Wan2.2 pull
 docker exec -e ENABLE_TORCH_PROFILER=1 zov_wan2.2_rope_alltoall_fusion_test bash /app/Wan2.2/tests/i2v.sh
-docker exec zov_wan2.2_rope_alltoall_fusion_test  7z a -mx=9 wan2.2_triton_optimized_with_call_stack.7z /app/Wan2.2/trace_rank* 
+# docker exec zov_wan2.2_rope_alltoall_fusion_test  7z a -mx=9 wan2.2_triton_optimized_with_call_stack.7z /app/Wan2.2/trace_rank* 
 # docker exec zov_wan2.2_rope_alltoall_fusion_test rm -rf /app/Wan2.2/trace_rank*
 # docker exec zov_wan2.2_rope_alltoall_fusion_test mv /app/wan2.2_triton_optimized_with_call_stack.7z /app/home/jialzhu
 
