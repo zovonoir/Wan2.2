c4cc351b62917b6a18f1028af2dc2a31269388af
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-11 12:26:06 +0800

    set determinism frequency test,modify monitor logic,set to 1500

--- run_monitor.sh
+++ run_monitor.sh
@@ -91,7 +91,7 @@ docker exec zov_wan2.2_rope_alltoall_fusion_test git -C /app/Wan2.2 pull
 start_monitor
 
 # ============= 模型运行阶段 =============
-sudo rocm-smi --setperfdeterminism 1000
+sudo rocm-smi --setperfdeterminism 1500
 docker exec -e ENABLE_TORCH_PROFILER=1 zov_wan2.2_rope_alltoall_fusion_test bash /app/Wan2.2/tests/i2v.sh
 
 # ============= 模型运行后停止监控 =============
