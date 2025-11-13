ea63d9c0d2cbd72642d3f26de12694f6feba3c04
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-13 18:28:24 +0800

    delete redundant comments

--- wan/distributed/triton_kernels.py
+++ wan/distributed/triton_kernels.py
@@ -16,11 +16,9 @@ def rope_triton_kernel_fp16(qk_ptr, freqs_ptr,
                     ):
     program_id = tl.program_id(0)
     tl.static_assert(PROG_SIZE > 0 and (PROG_SIZE & (PROG_SIZE - 1)) == 0,f"PROG_SIZE only support power of 2!current is {PROG_SIZE}")
-    freqs_rank_offset = PROG_SIZE * sp_rank * s_per_rank # freq当前rank在freq中的偏移量
-    freq_rank_start_ptr = freqs_ptr + freqs_rank_offset # 这个rank的起始指针
+    freqs_rank_offset = PROG_SIZE * sp_rank * s_per_rank
+    freq_rank_start_ptr = freqs_ptr + freqs_rank_offset
     freq_program_start_ptr = freq_rank_start_ptr + program_id * PROG_SIZE
-    # program_start = program_id * PROG_SIZE
-    # rank_process_size = PROG_SIZE * s_per_rank * head_num # 这个rank一共要处理这么多的数据
     even_offset = 2 * (tl.arange(0,PROG_SIZE) // 2) # program_start + [0,0,2,2,4,4,...126,126]
     odd_offset = even_offset + 1 # program_start + [1,1,3,3,5,5,...,127,127]
     freqs_offset = tl.arange(0,PROG_SIZE) #  [0,1,2,3,4,5...],freq只能load128个数据,并且复用
@@ -32,7 +30,7 @@ def rope_triton_kernel_fp16(qk_ptr, freqs_ptr,
     freqs_mask_swap = freqs_offset_swap < PROG_SIZE
     vy1_fp64 = tl.cast(tl.load(freq_program_start_ptr + freqs_offset,mask=freqs_mask,other=0.0),tl.float64)
     vy2_fp64 = tl.cast(tl.load(freq_program_start_ptr + freqs_offset_swap,mask=freqs_mask_swap,other=0.0),tl.float64)
-    vx1_ptr_block_even = qk_ptr + program_id * PROG_SIZE * head_num + even_offset # 当前BLOCK处理数据的第0个head的起始地址 + 每个数据偏移量
+    vx1_ptr_block_even = qk_ptr + program_id * PROG_SIZE * head_num + even_offset
     vx1_ptr_block_odd = qk_ptr + program_id * PROG_SIZE * head_num + odd_offset
     output_ptr_block = output_ptr + program_id * PROG_SIZE * head_num + tl.arange(0,PROG_SIZE)
     output_mask = tl.arange(0,PROG_SIZE) < PROG_SIZE
@@ -43,12 +41,12 @@ def rope_triton_kernel_fp16(qk_ptr, freqs_ptr,
     output_seq_len = input_seq_len * world_size
     # [1,input_seq_len,input_head_num,head_size] -> [1,input_seq_len * world_size,input_head_num / world_size,head_size]
 
-    for head_idx in tl.range(0,head_num,1): # 在head 维度循环,每个program计算一个token的数据量
+    for head_idx in tl.range(0,head_num,1):
         # load single head for x
         vx1_fp64 = tl.cast(tl.load(vx1_ptr_block_even + head_idx * PROG_SIZE,mask=even_mask,other=0.0),tl.float64) # mask可能不严谨
         vx2_fp64 = tl.cast(tl.load(vx1_ptr_block_odd + head_idx * PROG_SIZE,mask=odd_mask,other=0.0),tl.float64)
         # complex_output = tl.cast(vx1*vy1 + coe*vx2*vy2,tl.float32) # 一个head的输出
-        complex_output_bf16 = tl.cast(vx1_fp64*vy1_fp64 + coe*vx2_fp64*vy2_fp64,tl.bfloat16) # rope输出是float32格式,但是后续做alltoall之前又被转换成了bfloat16,因此这里提前转换再写入
+        complex_output_bf16 = tl.cast(vx1_fp64*vy1_fp64 + coe*vx2_fp64*vy2_fp64,tl.bfloat16) # NOTE: convert to bf16
         tl.store(output_ptr_block + head_idx * PROG_SIZE,complex_output_bf16,mask=output_mask)
 
 @triton.jit
@@ -65,8 +63,8 @@ def rope_alltoall_4D_bf16_forward(qk_ptr, freqs_ptr,
     # 每个program负责一个token,共40*128=5120个float32相乘
     program_id = tl.program_id(0)
     tl.static_assert(hs > 0 and (hs & (hs - 1)) == 0,f"PROG_SIZE only support power of 2!current is {hs}")
-    freqs_rank_offset = hs * local_rank * seq_len_per_rank # freq当前rank在freq中的偏移量
-    freq_rank_start_ptr = freqs_ptr + freqs_rank_offset # 这个rank的起始指针
+    freqs_rank_offset = hs * local_rank * seq_len_per_rank
+    freq_rank_start_ptr = freqs_ptr + freqs_rank_offset
     freq_program_start_ptr = freq_rank_start_ptr + program_id * hs
     even_offset = 2 * (tl.arange(0,hs) // 2) # program_start + [0,0,2,2,4,4,...126,126]
     odd_offset = even_offset + 1 # program_start + [1,1,3,3,5,5,...,127,127]
@@ -79,7 +77,7 @@ def rope_alltoall_4D_bf16_forward(qk_ptr, freqs_ptr,
     freqs_mask_swap = freqs_offset_swap < hs
     vy1_fp64 = tl.cast(tl.load(freq_program_start_ptr + freqs_offset,mask=freqs_mask,other=0.0),tl.float64)
     vy2_fp64 = tl.cast(tl.load(freq_program_start_ptr + freqs_offset_swap,mask=freqs_mask_swap,other=0.0),tl.float64)
-    vx1_ptr_block_even = qk_ptr + program_id * hs * in_hn + even_offset # 当前BLOCK处理数据的第0个head的起始地址 + 每个数据偏移量
+    vx1_ptr_block_even = qk_ptr + program_id * hs * in_hn + even_offset
     vx1_ptr_block_odd = qk_ptr + program_id * hs * in_hn + odd_offset
     output_mask = tl.arange(0,hs) < hs
     # alltoall 4D输出形状
