9bd588f919ec8448b7b5fba20f40d07efe3f9365
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-13 15:48:26 +0800

    restore to original to solve mismatch,step1,add iris buffer

--- generate.py
+++ generate.py
@@ -13,7 +13,7 @@ import random
 import torch
 import torch.distributed as dist
 from PIL import Image
-
+import iris
 import wan
 from wan.configs import MAX_AREA_CONFIGS, SIZE_CONFIGS, SUPPORTED_SIZES, WAN_CONFIGS
 from wan.distributed.util import init_distributed_group
@@ -330,6 +330,16 @@ def generate(args):
             init_method="env://",
             rank=rank,
             world_size=world_size)
+
+        shmem = iris.iris(1024*1024*1024*4) # 4G
+        all_to_all_buffer_q = shmem.zeros([1,13640*8,40//8,128],dtype=torch.bfloat16,device="cuda") # 这里的参数量后面再补充为可配置的
+        all_to_all_buffer_k = shmem.zeros([1,13640*8,40//8,128],dtype=torch.bfloat16,device="cuda")
+        all_to_all_buffer_v = shmem.zeros([1,13640*8,40//8,128],dtype=torch.bfloat16,device="cuda")
+        all_to_all_buffer_o = shmem.zeros([1,13640*8,40//8,128],dtype=torch.bfloat16,device="cuda")
+        attn_buffer = shmem.zeros([1,13640,40,128],dtype=torch.bfloat16,device="cuda")
+        iris_all_to_all_buffers = [all_to_all_buffer_q,all_to_all_buffer_k,all_to_all_buffer_v,all_to_all_buffer_o,attn_buffer]
+
+
     else:
         assert not (
             args.t5_fsdp or args.dit_fsdp
@@ -537,7 +547,9 @@ def generate(args):
             sampling_steps=args.sample_steps,
             guide_scale=args.sample_guide_scale,
             seed=args.base_seed,
-            offload_model=args.offload_model)
+            offload_model=args.offload_model,
+            iris_shm_handle = shmem, # 单卡情况下无法运行,后面需要加上条件判断
+            iris_buffer_list = iris_all_to_all_buffers)
 
     if rank == 0:
         if args.save_file is None:
