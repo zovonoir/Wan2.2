f627a470c14f996e792c55064f6acd6cc9339f5e
Zhu Jiale <Jiale.Zhu@amd.com>
2025-11-19 15:27:58 +0800

    modify running script

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-docker rm -f zov_wan2.2_rope_alltoall_fusion_test_shape
+docker rm -f zov_wan2.2_alltoall_nobarrier
 docker rmi wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2
 
 docker build -f dockerfile_rocm_7.0.2 -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2 .
@@ -14,9 +14,10 @@ docker run  -d \
         --ulimit memlock=-1 \
         --ulimit stack=67108864 \
         -v /home/jialzhu:/home/jialzhu \
-        --name zov_wan2.2_rope_alltoall_fusion_test_shape \
+        --name zov_wan2.2_alltoall_nobarrier \
         -t wan2.2_rocm7.0.2_pytorch_2.10.0.dev20251023_iris_0dfc460_2
 
-docker restart zov_wan2.2_rope_alltoall_fusion_test_shape
-docker exec zov_wan2.2_rope_alltoall_fusion_test_shape git -C /app/Wan2.2 pull
-docker exec zov_wan2.2_rope_alltoall_fusion_test_shape bash /app/Wan2.2/tests/i2v.sh
+docker restart zov_wan2.2_alltoall_nobarrier
+docker exec zov_wan2.2_alltoall_nobarrier git -C /app/Wan2.2 pull
+docker exec zov_wan2.2_alltoall_nobarrier git -C /app/Wan2.2 checkout develop/iris_no_sync_alltoall
+docker exec zov_wan2.2_alltoall_nobarrier bash /app/Wan2.2/tests/i2v.sh
